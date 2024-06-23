c27eb37da9ab8df77ce717de1a76722567957c8f
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-06-23 22:23:07 +0530

    made the layout of frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Database to Connected"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+  return res.json({
+    message: "hello",
+  });
+});
+
+app.listen(3000, () => console.log("Server started at PORT 3000"));
---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant={"ghost"}
+      className=" font-bold hover:text-orange-500 text-xl hover:bg-white"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
