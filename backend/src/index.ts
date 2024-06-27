e15b63654402b4ccbd492fe0a2fec9dcbf83e1a0
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-06-27 22:12:04 +0530

    added user model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-
+import myUserRoute from "./routes/MyUserRoute";
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Database to Connected"));
@@ -10,6 +10,8 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/my/user", myUserRoute);
 
 app.get("/", async (req: Request, res: Response) => {
   return res.json({
---
+++ backend/src/routes/MyUserRoute.ts
@@ -0,0 +1,5 @@
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+const router = express.Router();
+router.post("/", MyUserController.createCurrentUser);
+export default router;
