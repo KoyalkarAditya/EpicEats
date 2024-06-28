3f30937b2295750a18ea95e5d6ac79a37eea5890
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-06-28 11:46:13 +0530

    added create user mutaion

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,9 +1,11 @@
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateUser } from "../api/MyUserApi";
 
 type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser, isError, isLoading, isSuccess } = useCreateUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUrl = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -11,7 +13,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initiate auth");
   }
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log(user);
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
