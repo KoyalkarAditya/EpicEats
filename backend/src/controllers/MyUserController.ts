3f55a375174aa2b709d65899e7ae3992df7dcc6d
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-06-29 13:29:04 +0530

    added update user info functionality

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -17,7 +17,26 @@ const createCurrentUser = async (req: Request, res: Response) => {
     });
   }
 };
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User Not found" });
+    }
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+    await user.save();
+    return res.status(200).send(user);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: "Error Updating the user" });
+  }
+};
 
 export default {
   createCurrentUser,
+  updateCurrentUser,
 };
--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -2,18 +2,28 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallBackPage from "./pages/AuthCallBackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route
         path="/"
         element={
-          <Layout>
+          <Layout showHero={true}>
             <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallBackPage />} />
+      <Route
+        path="user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
