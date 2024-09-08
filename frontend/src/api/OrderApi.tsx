4eea36fb767c7ac3b186ec2c948cc643db867791
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-09-08 16:43:13 +0530

    order status polling

--- frontend/src/api/OrderApi.tsx
+++ frontend/src/api/OrderApi.tsx
@@ -76,7 +76,10 @@ export const useGetMyOrders = () => {
   };
   const { data: orders, isLoading } = useQuery(
     "fetchOrders",
-    getMyOrdersRequest
+    getMyOrdersRequest,
+    {
+      refetchInterval: 5000,
+    }
   );
   return { orders, isLoading };
 };
